71fe8f0798a2509e1d5593ae08135a1ecd4efeec
nemlyc_desktop <cf.m1t0k@gmail.com>
2019-10-31 20:47:28 +0900

    Add: コントロールクラスのガワ

---
+++ tdu_market/src/tdu_market/controller/ReferItemPage.java
@@ -0,0 +1,34 @@
+package tdu_market.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ReferItemPage
+ */
+@WebServlet("/ReferItemPage")
+public class ReferItemPage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReferItemPage() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.err.println("non implementation!");
+	}
+
+
+}
