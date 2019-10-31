3defe929abccffc20018208ee4b75ebbd1e00bea
Gamu2059 <yamagami0711syo@gmail.com>
2019-10-31 14:40:30 +0900

    Update : 少しだけダミーを追加

---
+++ tdu_market/src/tdu_market/entity_manager/AccountUtil.java
@@ -0,0 +1,34 @@
+package tdu_market.entity_manager;
+
+public final class AccountUtil {
+
+	public static boolean isMeetRequirementMailAddress( String mailAddress) {
+		System.err.println("non implementation!");
+		return false;
+	}
+
+	public static boolean isMeetRequirementPassword( String nonHashedPassword ) {
+		System.err.println("non implementation!");
+		return false;
+	}
+
+	public static String getHashedPassword( String nonHashedPassword) {
+		System.err.println("non implementation!");
+		return nonHashedPassword;
+	}
+
+	public static boolean isStudentMailAddress( String mailAddress) {
+		System.err.println("non implementation!");
+		return false;
+	}
+
+	public static boolean isManagerMailAddress( String mailAddress ) {
+		System.err.println("non implementation!");
+		return false;
+	}
+
+	public static String getStudentNumber( String mailAddress ) {
+		System.err.println("non implementation!");
+		return mailAddress;
+	}
+}
