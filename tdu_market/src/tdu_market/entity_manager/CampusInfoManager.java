49979cb077c8deffd711c0a120ab18ec1379a358
Gamu2059 <yamagami0711syo@gmail.com>
2019-11-08 16:50:43 +0900

    Update : CampusInfoManagerの実装

--- tdu_market/src/tdu_market/entity_manager/CampusInfoManager.java
+++ tdu_market/src/tdu_market/entity_manager/CampusInfoManager.java
@@ -2,17 +2,29 @@ package tdu_market.entity_manager;
 
 import java.util.ArrayList;
 
+import tdu_market.dao.CampusInfoDAO;
 import tdu_market.dto.CampusGetInfo;
+import tdu_market.entity_bean.CampusInfo;
 
 public final class CampusInfoManager {
 
 	public CampusGetInfo getCampusInfo(long campusID) {
-		System.err.println("getCampusInfo is non implementation!");
-		return null;
+
+		CampusInfoDAO campusInfoDAO = new CampusInfoDAO();
+		CampusInfo campusInfo = campusInfoDAO.getCampusInfo(campusID);
+		return CampusGetInfo.create(campusInfo);
 	}
 
 	public ArrayList<CampusGetInfo> getCampusInfoList() {
-		System.err.println("getCampusInfoList is non implementation!");
-		return null;
+
+		CampusInfoDAO campusInfoDAO = new CampusInfoDAO();
+		ArrayList<CampusInfo> list = campusInfoDAO.getAllCampusInfo();
+
+		ArrayList<CampusGetInfo> result = new ArrayList<CampusGetInfo>();
+		for (CampusInfo i : list) {
+			result.add(CampusGetInfo.create(i));
+		}
+
+		return result;
 	}
 }
