05dae48e70bd56a9a2a6f7da9fb6cda6c867dd85
yoyo1nko <56778229+yoyo1nko@users.noreply.github.com>
2019-11-12 00:06:14 +0900

    add: logout update:login

--- tdu_market/src/tdu_market/controller/Login.java
+++ tdu_market/src/tdu_market/controller/Login.java
@@ -1,5 +1,6 @@
 package tdu_market.controller;
-
+import tdu_market.entity_manager.ManagerInfoManager;
+import tdu_market.entity_manager.StudentInfoManager;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -23,18 +24,18 @@ import javax.servlet.http.HttpSession;
 @WebServlet("/Login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	//ここでは便宜的にアドレスとパスを与えている
-    private final String loginId = "1@gmail.com";
-    private final String password = "123";
-    
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Login() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private final String loginId = "1@gmail.com";
+	private final String password = "123";
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Login() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 
 
@@ -46,11 +47,34 @@ public class Login extends HttpServlet {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		System.err.println("Login is non implementation!");
 		
-	      String sentaddress = request.getParameter("mailaddress");
-	        String sentPw = request.getParameter("password");
-	       // System.out.println(sentPw);
-	        //System.out.println(sentaddress);
-	        
+
+		ManagerInfoManager manage = new ManagerInfoManager();
+		StudentInfoManager student = new StudentInfoManager();
+		String sentaddress = request.getParameter("mailaddress");
+		String sentPw = request.getParameter("password");
+		
+		// 'メールアドレスから学生か運営かを判定する
+		if (sentaddress.endsWith("@ms.dendai.ac.jp")) {
+			// '学生ならStudentInfoManagerにメールアドレスとパスワードを渡し、ログインできるかチェックする
+			//'アカウントが仮登録状態ならば、新規登録画面に遷移する
+			//'アカウントが登録済み状態ならば、トップ画面に遷移する
+			// 'チェックがOKなら、StudentInfoManagerのloginを呼ぶ
+			student.login();
+		}
+
+		if (sentaddress.endsWith("@ms.dendai.ac.jp")) {
+			// '運営ならManagerInfoManagerにメールアドレスとパスワードを渡し、ログインできるかチェックする
+			//'アカウントが仮登録状態ならば、新規登録画面に遷移する
+			//'アカウントが登録済み状態ならば、トップ画面に遷移する
+			// 'チェックがOKなら、ManagerInfoManagerのloginを呼ぶ
+		}
+
+		//セッションにメールアドレスを保存する(他にいい実装があるかもしれない...)
+		HttpSession session = request.getSession();
+		session.setAttribute("mailaddress", sentaddress);
+
+
+		/*作ってしまったログイン処理　
 	        if (sentaddress.equals(loginId) && sentPw.equals(password)) {
 	            HttpSession session = request.getSession();
 	            session.setAttribute("loginUser", true);
@@ -62,14 +86,16 @@ public class Login extends HttpServlet {
 	            RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
 	            rd.forward(request, response);
 	        }
-	    }
+		 */
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
-        rd.forward(request, response);
-    }
-    
 	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+		rd.forward(request, response);
+	}
+
+}
 
 
 
--- tdu_market/src/tdu_market/controller/Logout.java
+++ tdu_market/src/tdu_market/controller/Logout.java
@@ -1,7 +1,24 @@
 package tdu_market.controller;
-
+import tdu_market.entity_manager.StudentInfoManager;
 import java.io.IOException;
+import tdu_market.entity_manager.ManagerInfoManager;
+import tdu_market.entity_manager.StudentInfoManager;
+import java.io.IOException;
+import java.util.Enumeration;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletResponse;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,13 +32,13 @@ import javax.servlet.http.HttpServletResponse;
 public class Logout extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Logout() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Logout() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 
 	/**
@@ -30,6 +47,26 @@ public class Logout extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		System.err.println("Logout is non implementation!");
+		StudentInfoManager student = new StudentInfoManager();
+		
+		
+		// StudentInfoManagerのlogoutを呼ぶ
+		student.logout(mailAddress);
+		//セッションに保存してある色々なデータを抹消する
+		HttpSession sess = request.getSession();
+			//セッションにある全ての要素名を取得する
+		Enumeration vals = sess.getAttributeNames();
+
+			//取得した要素名をループ処理で全て削除する
+		while(vals.hasMoreElements()){
+			String nm = (String)vals.nextElement();
+			sess.removeAttribute(nm);
+		}	
+		//ログインページに遷移する
+		RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+		rd.forward(request, response);
+
+
 	}
 
 }
