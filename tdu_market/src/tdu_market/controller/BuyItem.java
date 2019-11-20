bbc82e1e3262fe3d118e6d51d602265c6f28b23f
yoyo1nko <56778229+yoyo1nko@users.noreply.github.com>
2019-11-21 00:55:11 +0900

    Add : BuyItem

--- tdu_market/src/tdu_market/controller/BuyItem.java
+++ tdu_market/src/tdu_market/controller/BuyItem.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import tdu_market.entity_manager.ItemInfoManager;
+import tdu_market.dto.ItemBuyInfo;
 import tdu_market.util.ControllerUtil;
 
 /**
@@ -18,13 +19,13 @@ import tdu_market.util.ControllerUtil;
 public class BuyItem extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public BuyItem() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public BuyItem() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 
 	/**
@@ -37,10 +38,12 @@ public class BuyItem extends HttpServlet {
 		if (!ControllerUtil.verifyLogin(request, response)) {
 			return;
 		}
-		
+
+
+		ItemBuyInfo buyInfo = new ItemBuyInfo(request.getParameter("beginTraderMailAddress"), Integer.valueOf(request.getParameter("tradedItemID")));
 		ItemInfoManager itemInfo = new ItemInfoManager();
 		//アイテム購入処理
-		//itemInfo.buyItem
+		itemInfo.BuyItem(buyInfo);
 	}
 
 }
