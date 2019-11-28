82abf1e63d9c2ee35d69eec5d4f2f1dc1afddd40
Gamu2059 <yamagami0711syo@gmail.com>
2019-11-28 19:33:32 +0900

    Update : 画像アップロードに対応

--- tdu_market/src/tdu_market/controller/UpdateStudentPage.java
+++ tdu_market/src/tdu_market/controller/UpdateStudentPage.java
@@ -2,7 +2,6 @@ package tdu_market.controller;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Collection;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -13,101 +12,55 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
 
+import tdu_market.dto.ReturnInfo;
 import tdu_market.dto.StudentUpdateInfo;
 import tdu_market.entity_manager.StudentInfoManager;
 import tdu_market.util.ControllerUtil;
-
-/**
- * Servlet implementation class UpdateStudentPage
- */
+import tdu_market.util.JspPath;
 
 @WebServlet("/tdu_market/controller/UpdateStudentPage")
-@MultipartConfig()
+@MultipartConfig(maxFileSize = 1024 * 1024)
 public class UpdateStudentPage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public UpdateStudentPage() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		System.err.println("UpdateStudentPage is non implementation!");
-
-		StudentInfoManager student = new StudentInfoManager();
 		if (!ControllerUtil.verifyLogin(request, response)) {
+			ControllerUtil.translatePage(JspPath.index, request, response);
 			return;
 		}
-		/* Part part = request.getPart("iconImageURL"); */
-		Collection<Part> parts = request.getParts();
 
-
-		/*ファイル名の取得getSubmittedFilename()
-		 * String fileName = this.getFileName(part); System.out.println(fileName);
-		 * part.write(getServletContext().getRealPath("WEB-INF/uploaded") + "/" +
-		 * fileName);
-		 */
-
-		//必要項目の入力チェック（jsp側）
+		request.setCharacterEncoding("UTF-8");
 		HttpSession session = request.getSession();
-		String mailAddress = (String)session.getAttribute("mailaddress");
+
+		//入力フォームの必要事項が入力されているかチェック
+		//ここではjsp側でしているため、していない
+		String mailAddress = ControllerUtil.getMailAddress(request, response);
 		String pass1 = request.getParameter("nonHashedPassword1");
 		String pass2 = request.getParameter("nonHashedPassword2");
 		String name = request.getParameter("displayName");
 		String intro = request.getParameter("selfIntroduction");
-//		String url = request.getParameter("iconImageURL");
-
 		Part part = request.getPart("iconImageURL");
 		InputStream is = part.getInputStream();
 
-
-		System.out.println(pass1);
-		System.out.println(name);
-		System.out.println(intro);
-
-		if(!pass1.equals(pass2)) {
-			ControllerUtil.translatePage("/tdu_market/Student/edit_profile_student.jsp", request, response);
+		if (!pass1.equals(pass2)) {
+			session.setAttribute("errorInfo", "パスワードが一致しません。");
+			ControllerUtil.translatePage(JspPath.edit_profile_student, request, response);
 			return;
 		}
 
 		// 学生自身は所属学科を変更できないため、subjectID = -1
-		// TODO urlは、後々バイナリで受け取る用の処理の変更する
-		StudentUpdateInfo updateInfo = new StudentUpdateInfo(
-				mailAddress,
-				pass1,
-				name,
-				-1,
-				intro,
-				is
-				);
-		student.updateStudentInfo(updateInfo);
+		StudentUpdateInfo updateInfo = new StudentUpdateInfo(mailAddress,pass1,name,-1,intro,is);
+		StudentInfoManager student = new StudentInfoManager();
+		ReturnInfo updateResult = student.updateStudentInfo(updateInfo);
 
-		//ページ遷移
-		ControllerUtil.translatePage("/tdu_market/Student/edit_profile_student.jsp", request, response);
+		if (updateResult.isSuccess()) {
+			session.setAttribute("errorInfo", null);
+		} else {
+			session.setAttribute("errorInfo", updateResult.getMsg());
+		}
+
+		ControllerUtil.translatePage(JspPath.edit_profile_student, request, response);
 	}
-
-    private String getFileName(Part part) {
-		/*
-		 * String name = null; for (String dispotion :
-		 * part.getHeader("Content-Disposition").split(";")) { if
-		 * (dispotion.trim().startsWith("filename")) { name =
-		 * dispotion.substring(dispotion.indexOf("=") + 1).replace("\"", "").trim();
-		 * name = name.substring(name.lastIndexOf("\\") + 1); break; } } return name;
-		 */
-        for (String cd : part.getHeader("Content-Disposition").split(";")) {
-            if (cd.trim().startsWith("filename")) {
-                return cd.substring(cd.indexOf('=') + 1).trim().replace("\"", "");
-            }
-        }
-        return null;
-    }
-
 }
