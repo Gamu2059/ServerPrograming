e854cb4cf33379289021212938c2107afb09a065
21w <akimoto1603401@gmail.com>
2019-11-30 20:46:07 +0900

    Update：取得データをシラバス情報も取得できるように変更

--- tdu_market/src/tdu_market/controller/ManagerReferItemPage.java
+++ tdu_market/src/tdu_market/controller/ManagerReferItemPage.java
@@ -1,6 +1,7 @@
 package tdu_market.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,8 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import tdu_market.dto.ItemGetInfo;
-import tdu_market.entity_manager.ItemInfoManager;
+import tdu_market.dto.RelatedClassGetInfo;
+import tdu_market.entity_manager.RelatedClassInfoManager;
 import tdu_market.util.ControllerUtil;
 import tdu_market.util.JspPath;
 
@@ -41,12 +42,12 @@ public class ManagerReferItemPage extends HttpServlet {
 			return;
 		}
 
-		ItemInfoManager itemInfo = new ItemInfoManager();
+		RelatedClassInfoManager itemInfoManager = new RelatedClassInfoManager();
 		//出品商品情報をリストへ保持
-		ItemGetInfo itemGetInfo = itemInfo.getItemInfo(Integer.valueOf(request.getParameter("itemID")).longValue()) ;
+		ArrayList<RelatedClassGetInfo> itemAndClassInfo = itemInfoManager.getRelatedClassInfoWithItem(Integer.valueOf(request.getParameter("itemID")).longValue());
 		//jspに情報を投げる。
 		HttpSession session = request.getSession();
-		session.setAttribute("itemInfo", itemGetInfo);
+		session.setAttribute("itemInfo", itemAndClassInfo);
 
 		//遷移
 		ControllerUtil.translatePage(JspPath.reference_item_detail_by_admin, request, response);
