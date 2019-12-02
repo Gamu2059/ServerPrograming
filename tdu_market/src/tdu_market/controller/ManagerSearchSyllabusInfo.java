c7a1f2c1ac16ceb717cc5d460688128e531d362f
21w <akimoto1603401@gmail.com>
2019-12-02 17:39:36 +0900

    Update：シラバス検索機能の実装

--- tdu_market/src/tdu_market/controller/ManagerSearchSyllabusInfo.java
+++ tdu_market/src/tdu_market/controller/ManagerSearchSyllabusInfo.java
@@ -1,13 +1,18 @@
 package tdu_market.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import tdu_market.dto.SyllabusGetInfo;
+import tdu_market.dto.SyllabusSearchInfo;
+import tdu_market.entity_manager.SyllabusInfoManager;
 import tdu_market.util.ControllerUtil;
 import tdu_market.util.JspPath;
 
@@ -37,6 +42,27 @@ public class ManagerSearchSyllabusInfo extends HttpServlet {
 			ControllerUtil.translatePage(JspPath.index, request, response);
 			return;
 		}
+
+		//文字コードを変換する
+		request.setCharacterEncoding("UTF-8");
+
+		//検索データを取得する
+		String classCodeKeyword = request.getParameter("classCodeKeyword");
+		long subjectID = Integer.valueOf(request.getParameter("subjectID")) ;
+		String classNameKeyword = request.getParameter("classNameKeyword");
+		String teacherNameKeyword = request.getParameter("teacherNameKeyword");
+		long semesterID = Integer.valueOf(request.getParameter("semesterID"));
+
+		//検索を行う
+		SyllabusInfoManager syllabusInfoManager = new SyllabusInfoManager();
+		SyllabusSearchInfo syllabusSearchInfo = new SyllabusSearchInfo(classCodeKeyword, subjectID, classNameKeyword, teacherNameKeyword, semesterID);
+
+		//検索結果を出力する
+		ArrayList<SyllabusGetInfo> syllabusGetInfos = syllabusInfoManager.searchSyllabus(syllabusSearchInfo);
+
+		//検索結果をjspに送信する
+		HttpSession session = request.getSession();
+		session.setAttribute("syllabusInfoList", syllabusGetInfos);
 
 		//遷移
 		ControllerUtil.translatePage(JspPath.reference_item_list_by_admin, request, response);
