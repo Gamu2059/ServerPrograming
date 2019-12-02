cc695cad3a4c0a44e5b6b926338e2c4c5facaa19
21w <akimoto1603401@gmail.com>
2019-12-02 16:40:18 +0900

    Add：ManagerSearchSyllabusInfo.javaの追加

---
+++ tdu_market/src/tdu_market/controller/ManagerSearchSyllabusInfo.java
@@ -0,0 +1,51 @@
+package tdu_market.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import tdu_market.entity_manager.ItemInfoManager;
+import tdu_market.util.ControllerUtil;
+import tdu_market.util.JspPath;
+
+/**
+ * Servlet implementation class ManagerSearchSyllabusInfo
+ */
+@WebServlet("/tdu_market/controller/ManagerSearchSyllabusInfo")
+public class ManagerSearchSyllabusInfo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ManagerSearchSyllabusInfo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.err.println("ManagerDeleteItemInfo is non implementation!");
+
+		if (!ControllerUtil.verifyLogin(request, response)) {
+			ControllerUtil.translatePage(JspPath.index, request, response);
+			return;
+		}
+		//削除処理
+		ItemInfoManager itemInfo = new ItemInfoManager();
+		itemInfo.deleteItemInfo(Integer.valueOf(request.getParameter("itemID")));
+
+		//遷移
+		ControllerUtil.translatePage(JspPath.reference_item_list_by_admin, request, response);
+
+	}
+
+}
