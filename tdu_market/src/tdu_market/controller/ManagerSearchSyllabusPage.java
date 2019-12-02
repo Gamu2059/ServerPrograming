d9b1d197df7bcb25e6c7bc633e0b34866bce3749
21w <akimoto1603401@gmail.com>
2019-12-02 16:37:03 +0900

    Add：ManagerSearchSyllabusPage.javaを生成

---
+++ tdu_market/src/tdu_market/controller/ManagerSearchSyllabusPage.java
@@ -0,0 +1,44 @@
+package tdu_market.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import tdu_market.util.ControllerUtil;
+import tdu_market.util.JspPath;
+
+/**
+ * Servlet implementation class SearchSyllabusPage
+ */
+@WebServlet("/tdu_market/controller/ManagerSearchSyllabusPage")
+public class ManagerSearchSyllabusPage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ManagerSearchSyllabusPage() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.err.println("SearchSyllabusPage is non implementation!");
+
+		if (!ControllerUtil.verifyLogin(request, response)) {
+			ControllerUtil.translatePage(JspPath.index, request, response);
+			return;
+		}
+		//遷移
+		ControllerUtil.translatePage(JspPath.search_from_syllabus, request, response);
+
+	}
+}
